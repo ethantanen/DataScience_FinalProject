f14c8b91a8419748bfb7a7ecf9628542f6ced301
ethan <ethantanen@yahoo.com>
2018-04-29 23:15:00 -0600

    organized repo into scraping and dataset folders

---
+++ Scraping/top200_scrape/get_top200_charts.sh
@@ -0,0 +1,11 @@
+#Download all available dates from the spotify charts site by iterating through the dates stored in dates.txt
+
+a="https://spotifycharts.com/regional/us/daily/"
+b="/download"
+
+for i in $(cat dates.txt)
+do
+    echo $a$i$b
+    wget -O "data/"$i".txt" $a$i$b
+
+done
